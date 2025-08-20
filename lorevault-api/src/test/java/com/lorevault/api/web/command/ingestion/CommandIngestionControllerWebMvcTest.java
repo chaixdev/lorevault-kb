b086761324cf5d7ff9d70dfb1c6cff65c8242d0e
chai <bhagwat.chaitanya@gmail.com>
2025-08-20 15:38:07 +0000

    refactor(test): replace deprecated @MockBean with @MockitoBean
    - Update imports from o.s.b.test.mock.mockito.MockBean to o.s.test.context.bean.override.mockito.MockitoBean
    - Replace @MockBean with @MockitoBean across all WebMvc test files:
      - AskControllerWebMvcTest: 2 annotations updated
      - HealthControllerWebMvcTest: 3 annotations updated
      - JobsControllerWebMvcTest: 1 annotation updated
      - CommandIngestionControllerWebMvcTest: 5 annotations updated
    - Spring Boot 3.5.4 compatible modern test bean override mechanism
    - All 24 WebMvc tests passing, no deprecation warnings

--- lorevault-api/src/test/java/com/lorevault/api/web/command/ingestion/CommandIngestionControllerWebMvcTest.java
+++ lorevault-api/src/test/java/com/lorevault/api/web/command/ingestion/CommandIngestionControllerWebMvcTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -34,19 +34,19 @@ class CommandIngestionControllerWebMvcTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     IngestionService ingestionService;
 
-    @MockBean
+    @MockitoBean
     FileUploadValidator fileUploadValidator;
 
-    @MockBean
+    @MockitoBean
     CoordinatesBuilder coordinatesBuilder;
 
-    @MockBean
+    @MockitoBean
     ErrorResponseFactory errorResponseFactory;
 
-    @MockBean
+    @MockitoBean
     FileContentExtractor fileContentExtractor;
 
     @AfterEach
--- lorevault-api/src/test/java/com/lorevault/api/web/query/job/JobsControllerWebMvcTest.java
+++ lorevault-api/src/test/java/com/lorevault/api/web/query/job/JobsControllerWebMvcTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +30,7 @@ class JobsControllerWebMvcTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     IngestionService ingestionService;
 
     @AfterEach
