70aa678363f44a3bde28d98dd94bf6f97a34532e
chai <bhagwat.chaitanya@gmail.com>
2025-10-14 23:41:21 +0000

    update state

--- lorevault-api/src/main/java/com/lorevault/api/service/content/ChunkService.java
+++
@@ -1,4 +0,0 @@
-package com.lorevault.api.service.content;
-
-// Removed: Deprecated JPA-based ChunkService eliminated after Neo4j migration.
-// File retained temporarily as tombstone; delete in subsequent cleanup commit if desired.
